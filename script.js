a14bff4f1d5745749473456d3fc6b2e97333c08d
NikolozMaghlakelidze <nikolozmaghlakelidze10@gmail.com>
2024-02-02 16:49:41 +0400

    updated

--- script.js
+++ script.js
@@ -21,57 +21,6 @@ let dasatvleliasoebi = asoebi.reduce((gamotvla, aso) => {
 
 console.log(dasatvleliasoebi); 
 
-class Car {
-  constructor(brand, model) {
-      this.brand = brand;
-      this.model = model;
-      this.speed = 0;
-      this.motion = "This car is not moving";
-  }
-
-  check_motion() {
-      if (this.speed > 0) {
-          this.motion = "This car is moving";
-      } else {
-          this.motion = "This car is stopped";
-      }
-  }
-
-  accelerate(speed) {
-      this.speed += speed;
-      if (this.speed > 100) { 
-          this.speed = 100;
-      }
-      this.check_motion();
-  }
-
-  brake(speed) {
-      this.speed -= speed;
-      if (this.speed < 0) {
-          this.speed = 0;
-      }
-      this.check_motion();
-  }
-
-  emergency_brake() {
-      this.speed = 0;
-      this.check_motion();
-  }
-
-  status() {
-      return `Vehicle ${this.brand} ${this.model} is moving at ${this.speed} km/h and status is ${this.motion}.`;
-  }
-}
-
-let car = new Car('kia', 'ferrari');
-console.log(car.status());
-car.accelerate(10);
-console.log(car.status());
-car.brake(40);
-console.log(car.status());
-car.emergency_brake(20);
-console.log(car.status());
-
 function addAsync(num1, num2) {
   return new Promise((resolve, reject) => {
     if (typeof num1 !== 'number' || num1 === undefined || typeof num2 !== 'number' || num2 === undefined) {
